b624bce8e5d006e22cbc8799bcb0a4c7824ee5ca
Mikhail Tikhonov <wbe7@ya.ru>
2019-09-13 13:34:29 +0300

    Add ip

--- main.go
+++ main.go
@@ -31,5 +31,21 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	for _, a := range addrs {
 		fmt.Print(w, a)
 	}  
-	fmt.Fprint(w, a)
+	fmt.Fprint(w, GetLocalIP)
+}
+
+func GetLocalIP() string {
+    addrs, err := net.InterfaceAddrs()
+    if err != nil {
+        return ""
+    }
+    for _, address := range addrs {
+        // check the address type and if it is not a loopback the display it
+        if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+            if ipnet.IP.To4() != nil {
+                return ipnet.IP.String()
+            }
+        }
+    }
+    return ""
 }
