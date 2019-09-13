09c951e32013f1be77eadb6b386fec1b5d07334f
Mikhail Tikhonov <wbe7@ya.ru>
2019-09-13 12:20:33 +0300

    Add ip

--- main.go
+++ main.go
@@ -9,10 +9,15 @@ import (
 const webContent = "Hello World! Coliges!"
 
 func main() {
+	addrs, err := net.LookupHost(name)
+	if err != nil {
+		fmt.Printf("Oops: %v\n", err)
+		return
+	}
 	http.HandleFunc("/", helloHandler)
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, webContent)
+	fmt.Fprint(w, webContent, addrs)
 }
